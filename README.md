63574ac8be674e0ca44a7243f3b256279a1b836d
Ev Bogue <ev@evbogue.com>
2023-09-24 09:56:43 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Deno Mini Chat
+
+This is an updated fork of https://dash.deno.com/playground/mini-ws-chat discussed on hn https://news.ycombinator.com/item?id=29131751
+
+```
+deno run --allow-all --unstable serve.js
+```
+
+or visit https://deno-mini-chat.deno.dev
---
+++ serve.js
@@ -0,0 +1,23 @@
+const html = `<script>const ws = new WebSocket((window.location.protocol === 'https:' ? 'wss://' : 'ws://') + location.host)
+  ws.onmessage = e => pre.textContent = e.data + '\\n' + pre.textContent</script>
+  <input onkeyup="event.key=='Enter'&&ws.send(this.value)"><pre id=pre>`
+
+const sockets = new Set()
+const channel = new BroadcastChannel("")
+
+channel.onmessage = e => {
+  channel.postMessage(e.data)
+  sockets.forEach(s => s.send(e.data))
+}
+
+Deno.serve((r) => {
+  try {
+    const { socket, response } = Deno.upgradeWebSocket(r)
+    sockets.add(socket)
+    socket.onmessage = channel.onmessage
+    socket.onclose = _ => sockets.delete(socket)
+    return response
+  } catch {
+    return new Response(html, {headers: {'Content-type': 'text/html'}})
+  }
+})
